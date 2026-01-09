0a6be1b0e692f9d8ee8c4a96187750432141d524
Hazyshades <30349843+Hazyshades@users.noreply.github.com>
2026-01-09 21:18:42 +0300

    Update NewsPanel.tsx

--- components/NewsPanel.tsx
+++ components/NewsPanel.tsx
@@ -19,26 +19,21 @@ interface NewsItem {
 const newsItems: NewsItem[] = [
 
   {
+    
+      id: '2',
+      title: 'Synchronization completed',
+      description: 'All data in the leaderboard has been synchronized on block 20804073. Next synchronization will be done each 1000 blocks.',
+      date: '1/9/2026',
+      type: 'update',
+    },
+    {
     id: '1',
     title: 'Creating cards from an Internal wallet',
     description: 'The new functionality allows you to select an Internal wallet for creating cards.',
     date: '1/3/2026',
     type: 'update',
-  },
-  {
-    id: '2',
-    title: 'Synchronization of blockchain data in the leaderboard',
-    description: 'Currently, some wallets are not displayed in the leaderboard. Data synchronization will happen soon.',
-    date: '1/3/2026',
-    type: 'announcement',
-  },
-  {
-    id: '3',
-    title: 'Fixed 403 error with creating new cards',
-    description: 'You can now create cards without any problems.',
-    date: '1/3/2026',
-    type: 'fixed',
-  },
+  }
+ 
 
   
 ];
